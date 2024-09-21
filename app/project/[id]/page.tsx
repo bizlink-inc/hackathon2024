b6d27c6b576b2bb457078e360c98d5faa239094f
shunbizlink <shunsuke.satou@bizlink.io>
2024-09-21 22:24:28 +0900

    add:detailをdbデータから取得

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -1,9 +1,8 @@
 import { Page } from "@/components/ProjectIdPage";
+import { getProjectDetail } from "@/lib/prisma/project/getDetail";
 
-export default function Project() {
-  return  (
-    <div>
-      <Page />
-    </div>
-  )
+export default async function Project({ params }: { params: { id: string } }) {
+  const projectId = parseInt(params.id, 10);
+  const project = await getProjectDetail(projectId);
+  return <Page project={project} />;
 }
